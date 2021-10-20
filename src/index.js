a197d86ab95d7e6ff770b25e8f4783484a5e2ff8
Zachary Moh <zacharymoh11@gmail.com>
2021-10-20 15:01:05 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state={ lat: null };
-    }
-    render() {
-
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
-
-        return <div>Latitude: {this.state.lat}</div>
-    }
+if (module.hot) {
+  module.hot.accept();
 }
 
-ReactDOM.render(
-    <App />, document.querySelector('#root')
-);
+class App extends React.Component {
 
+  state = { lat: null, errorMessage: ''};
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => this.setState({ lat: position.coords.latitude }),
+        (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+ 
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat}/>
+    }
+
+    return <div>Loading!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
